08d66c4bc76c1c05ecf9cca50ccbca0e456a2ddf
Chen X.W <xwc@zju.edu.cn>
2023-07-26 08:39:24 +0800

    init

---
+++ usb/bluetooth_usb_driver/.modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/home/cxw/Documents/BT5.0_Linux/20201202_LINUX_BT_DRIVER/usb/bluetooth_usb_driver/modules.order := {   echo /home/cxw/Documents/BT5.0_Linux/20201202_LINUX_BT_DRIVER/usb/bluetooth_usb_driver/rtk_btusb.ko; :; } | awk '!x[$$0]++' - > /home/cxw/Documents/BT5.0_Linux/20201202_LINUX_BT_DRIVER/usb/bluetooth_usb_driver/modules.order
